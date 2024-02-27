75a5be79b2d48e1b821890588a604186ee333b2b
nikunj bhuva <nikunjbhuva121@gmail.com>
2024-02-27 10:54:59 +0530

    hii

---
+++ index.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //wap to program print 1 to 10 digits.//
+
+    // int i=1;
+    // while(i<=10)
+    // {
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    // int i=1;
+    // do
+    // {
+    //   cout<<i<<endl;
+    //   i++;
+    // }while(i<=10);
+
+    // for(int i=1; i<=10; i++)
+    // {
+    //     cout << i << endl;
+    // }
+
+    //reverse loop//
+
+    // for(int i=10; i>=1; i--)
+    // {
+    //     cout << i << endl;
+    // }
+
+    // even numbers 1 to 10 with for loop//
+
+    for(int i=0; i<10; i++)
+    {
+        if(i%2==0)
+        {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
