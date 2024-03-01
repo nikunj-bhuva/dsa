7e687a27584eb483da83efe72886755a6c1b59f3
nikunj bhuva <nikunjbhuva121@gmail.com>
2024-03-01 17:30:25 +0530

    lec-5.3

---
+++ lec-5.3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int i;
+    // int n;
+    // cout << "enter n number : " << endl;
+    // cin >> n;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout << n << "x" << i << "=" << n * i << endl;
+    // }
+
+    // num of all digits //
+
+    
+
+    return 0;
+}
