247f1c64e37ce0f4522f0ec12ce9a0272b3d30f1
nikunj bhuva <nikunjbhuva121@gmail.com>
2024-03-01 17:28:39 +0530

    lec-5.2

---
+++ lec-5.2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int n;
+    // cout << "enter n number " << endl;
+    // cin >> n;
+    // int sum=0;
+
+    // int i=1;
+    // while(i<=n)
+    // {
+    //     sum=sum+i;
+    // }
+    // cout <<sum;
+
+// factorial of a numbers //
+
+    // int i,n,fact;
+    // fact=1;
+    // cout << "enter any number ;" << endl;
+    // cin >> n;
+    // i=n;
+    // while(i>=1)
+    // {
+    //     fact=fact*i;
+    // }
+    // cout << fact;
+
+    // factors of a numbers //
+
+    // int i,n;
+    // cout << "enter any number : ";
+    // cin>>n;
+
+    // i=1;
+    // while(i<=n)
+    // {
+    //     if(n%2==0)
+    //     {
+    //         cout << i << endl;
+    //     }
+    //     i++;
+    // }
+
+
+    return 0;
+}
